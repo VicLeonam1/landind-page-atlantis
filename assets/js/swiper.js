e81085afa7993224d8548b8cf4f2b7059b671b8d
Victor Leonam <169005086+VicLeonam1@users.noreply.github.com>
2024-11-30 20:30:00 -0300

    asas

--- assets/js/swiper.js
+++ assets/js/swiper.js
@@ -1,16 +1,18 @@
 const swiper = new Swiper('.slider-wrapper', {
-    loop: false,
-    grabCursor: true,
-    spaceBetween: 20,
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
-      dynamicBullets: true
-    },
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
+  grabCursor: true,
+  spaceBetween: 20,
+  slidesPerView: 2, // Exibe 2 slides de cada vez
+  slidesPerGroup: 2, // Move 2 slides ao mesmo tempo
+  loop: false, // Desativa o loop para limitar os slides
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    dynamicBullets: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
     breakpoints: {
       0: {
         slidesPerView: 1,
