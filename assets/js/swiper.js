da00e595081080228e0dac839c1483de10aa3ffa
Victor Leonam <169005086+VicLeonam1@users.noreply.github.com>
2024-12-04 16:30:08 -0300

    fo

--- assets/js/swiper.js
+++ assets/js/swiper.js
@@ -1,8 +1,6 @@
 const swiper = new Swiper('.slider-wrapper', {
   grabCursor: true,
   spaceBetween: 20,
-  slidesPerView: 2, // Exibe 2 slides de cada vez
-  slidesPerGroup: 2, // Move 2 slides ao mesmo tempo
   loop: false, // Desativa o loop para limitar os slides
   pagination: {
     el: '.swiper-pagination',
